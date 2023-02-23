353a60bceff4a405c05f2ea4ecb754e62bbc1296
adib3006 <ma.adib3006@gmail.com>
2023-02-23 10:56:11 +0600

    first commit

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,156 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+    createUserWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+} from "firebase/auth";
+import auth from "../../firebase/firebase.config";
+
+const initialState = {
+    user: { email: "", role: "" },
+    isLoading: true,
+    isError: false,
+    error: "",
+};
+
+export const createUser = createAsyncThunk(
+    "auth/createUser",
+    async ({ email, password }) => {
+        const data = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+        return data.user.email;
+    }
+);
+
+export const getUser = createAsyncThunk("auth/getUser", async (email) => {
+    const res = await fetch(`${process.env.REACT_APP_DEV_URL}/user/${email}`);
+    const data = await res.json();
+
+    if (data.status) {
+        return data;
+    }
+
+    return email;
+});
+
+export const loginUser = createAsyncThunk(
+    "auth/loginUser",
+    async ({ email, password }) => {
+        const data = await signInWithEmailAndPassword(auth, email, password);
+
+        return data.user.email;
+    }
+);
+
+export const googleLogin = createAsyncThunk("auth/googleLogin", async () => {
+    const googleProvider = new GoogleAuthProvider();
+
+    const data = await signInWithPopup(auth, googleProvider);
+
+    return data.user.email;
+});
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        logout: (state) => {
+            state.user = { email: "", role: "" };
+        },
+        setUser: (state, { payload }) => {
+            state.user.email = payload;
+            state.isLoading = false;
+        },
+        toggleLoading: (state) => {
+            state.isLoading = false;
+        },
+        clearError: (state) => {
+            state.isError = false;
+            state.error = "";
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(createUser.pending, (state) => {
+                state.isLoading = true;
+                state.isError = false;
+                state.error = "";
+            })
+            .addCase(createUser.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.user.email = action.payload;
+                state.isError = false;
+                state.error = "";
+            })
+            .addCase(createUser.rejected, (state, action) => {
+                state.isLoading = true;
+                state.user.email = "";
+                state.isError = true;
+                state.error = action.error.message;
+            })
+            .addCase(loginUser.pending, (state) => {
+                state.isLoading = true;
+                state.isError = false;
+                state.error = "";
+            })
+            .addCase(loginUser.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.user.email = action.payload;
+                state.isError = false;
+                state.error = "";
+            })
+            .addCase(loginUser.rejected, (state, action) => {
+                state.isLoading = true;
+                state.user.email = "";
+                state.isError = true;
+                state.error = action.error.message;
+            })
+            .addCase(googleLogin.pending, (state) => {
+                state.isLoading = true;
+                state.isError = false;
+                state.error = "";
+            })
+            .addCase(googleLogin.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.user.email = action.payload;
+                state.isError = false;
+                state.error = "";
+            })
+            .addCase(googleLogin.rejected, (state, action) => {
+                state.isLoading = true;
+                state.user.email = "";
+                state.isError = true;
+                state.error = action.error.message;
+            })
+            .addCase(getUser.pending, (state) => {
+                state.isLoading = true;
+                state.isError = false;
+                state.error = "";
+            })
+            .addCase(getUser.fulfilled, (state, action) => {
+                state.isLoading = false;
+                if (action.payload.status) {
+                    state.user = action.payload.data;
+                } else {
+                    state.user.email = action.payload;
+                }
+                state.isError = false;
+                state.error = "";
+            })
+            .addCase(getUser.rejected, (state, action) => {
+                state.isLoading = true;
+                state.user.email = "";
+                state.isError = true;
+                state.error = action.error.message;
+            });
+    },
+});
+
+export const { logout, setUser, toggleLoading, clearError } = authSlice.actions;
+
+export default authSlice.reducer;
---
+++ src/features/job/jobApi.js
@@ -0,0 +1,62 @@
+import apiSlice from "../api/apiSlice";
+
+const jobApi = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        postJob: builder.mutation({
+            query: (data) => ({
+                url: "/job",
+                method: "POST",
+                body: data,
+            }),
+            invalidatesTags: ["jobs"],
+        }),
+        apply: builder.mutation({
+            query: (data) => ({
+                url: "/apply",
+                method: "PATCH",
+                body: data,
+            }),
+        }),
+        question: builder.mutation({
+            query: (data) => ({
+                url: "/query",
+                method: "PATCH",
+                body: data,
+            }),
+        }),
+        reply: builder.mutation({
+            query: (data) => ({
+                url: "/reply",
+                method: "PATCH",
+                body: data,
+            }),
+        }),
+        getJob: builder.query({
+            query: () => ({
+                url: "/jobs",
+            }),
+            providesTags: ["jobs"],
+        }),
+        getAppliedJobs: builder.query({
+            query: (email) => ({
+                url: `/applied-jobs/${email}`,
+            }),
+            providesTags: ["jobs"],
+        }),
+        jobById: builder.query({
+            query: (id) => ({
+                url: `/job/${id}`,
+            }),
+        }),
+    }),
+});
+
+export const {
+    usePostJobMutation,
+    useGetJobQuery,
+    useJobByIdQuery,
+    useApplyMutation,
+    useGetAppliedJobsQuery,
+    useQuestionMutation,
+    useReplyMutation,
+} = jobApi;
